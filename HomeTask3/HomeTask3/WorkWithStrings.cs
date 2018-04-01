0e8802dad1965ec9c3fa1c3ef9e749f5ecb2f5d6
Alex <proskuraaleksandr93@gmail.com>
2018-04-01 20:11:31 +0300

    do not consider empty spaces between words

--- HomeTask3/HomeTask3/WorkWithStrings.cs
+++ HomeTask3/HomeTask3/WorkWithStrings.cs
@@ -8,16 +8,13 @@ namespace HomeTask3
         string text;
         int longestSentence;
 
-        public WorkWithStrings()
-        {
-            //text = "Here comes a traveller.Let us see who can strip him of his cloak?Said the Sun.";
-            Console.WriteLine("Write the text\n");
-            text = Console.ReadLine();
-        }
-
         public void LongestSentence()
         {
             Console.WriteLine("\n***Fun with sentences***\n");
+
+            //text = "Here comes a traveller.Let us see who can strip him of his cloak?Said the Sun.";
+            Console.WriteLine("Write the text\n");
+            text = Console.ReadLine();
 
             //split text to sentences
             string[] sentences = text.Split(new char[] { '.', '!', '?' });
@@ -28,21 +25,13 @@ namespace HomeTask3
             //count number of words in each sentence
             for (int i = 0; i < sentences.Length; i++)
             {
-                string[] words = sentences[i].Trim().Split(new char[] { ' ' });
-
+                string[] words = sentences[i].Trim().Split(new string[] { " ","  ","   ","    ","     " }, StringSplitOptions.RemoveEmptyEntries);
                 intArray[i] = words.Length;
             }
 
-            //The largest element of the counting array of words
+            //sort elements and get first - largest!
             longestSentence = intArray.OrderByDescending(x => x).First();
-            Console.WriteLine("The longest sentence in the text has {0} words",longestSentence);
-        }
-
-        //demo version of string
-        public override string ToString()
-        {
-            return longestSentence.ToString();
+            Console.WriteLine($"The longest sentence in the text has {longestSentence} words\a");
         }
     }
-
 }
